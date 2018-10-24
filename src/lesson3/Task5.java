8238dab3f1fa69069867968294fedb585db5a300
sdse92 <sdse92@gmail.com>
2018-10-24 12:57:45 +0300

    5 задача

--- src/lesson3/Task5.java
+++ src/lesson3/Task5.java
@@ -1,5 +1,6 @@
 package lesson3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*1) Найти в строке указанную подстроку и заменить ее на новую.
@@ -9,21 +10,27 @@ public class Task5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите строку ");
         String a = sc.nextLine();
-        System.out.println("Введите подстроку, которую нужно заменить ");
-        String b = sc.nextLine();
-        System.out.println("Введите подстроку, которую нужно вписать");
-        String c = sc.nextLine();
-        System.out.println(a.replace(b, c));
+//        System.out.println("Введите подстроку, которую нужно заменить ");
+//        String b = sc.nextLine();
+//        System.out.println("Введите подстроку, которую нужно вписать");
+//        String c = sc.nextLine();
+//        a = a.replace(b, c);
+        System.out.println(a);
 /*
         Требуется удалить из нее повторяющиеся символы и все пробелы.
         Например, если было введено "abc cde def", то должно быть выведено "abcdef".
 */
-        String [] strArr = {a};
-        for (int i = 0; i < a.length(); i++){
-            char ch = a.charAt(i);
-            if(ch == a.charAt(i+1)){
-                a = a.replace(a.charAt(i+1), "");
+        a = a.replaceAll("\\s+","");
+        StringBuffer strBuf = new StringBuffer(a);
+        for(int i = 0; i < strBuf.length(); i++){
+            char n = strBuf.charAt(i);
+            for(int j = 1; j < strBuf.length();j++){
+                if(strBuf.charAt(j) == n){
+                    strBuf.deleteCharAt(j);
+                }
             }
         }
+        a = strBuf.toString();
+        System.out.println(a);
     }
 }
