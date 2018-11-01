39fa35f11f990185a68b05f3eb5c18d7a898852c
sdse92 <sdse92@gmail.com>
2018-11-01 14:06:58 +0300

    LinkedList дописан push

---
+++ src/TasksLesson6/Task1/Element.java
@@ -0,0 +1,35 @@
+package TasksLesson6.Task1;
+
+public class Element {
+    private Object name;
+    private int index;
+    private Object next;
+
+    public Element(Object name) {
+        this.name = name;
+    }
+
+    public Object getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Object getNext() {
+        return next;
+    }
+
+    public void setNext(Object next) {
+        this.next = next;
+    }
+}
--- src/TasksLesson6/Task1/LinkedList.java
+++ src/TasksLesson6/Task1/LinkedList.java
@@ -1,18 +1,37 @@
 package TasksLesson6.Task1;
 
-public class LinkedList extends Object implements Stack {
+public class LinkedList implements Stack {
 
     private Object first;
     private Object last;
+    private Object current;
+    private int size;
 
-    public LinkedList(Object obj, int index) {
-        super(obj, index);
+    public void print(){
+        Object j = first;
+        while (j != null){
+            System.out.println(j + j.getName());
+            j = j.getNext();
+        }
     }
-
 
     @Override
     public void push(Object obj) {
-        Object n = new Object(obj, setIndex(););
+        Element e = new Element(obj);
+        if(last == null){
+            first = e;
+            last = e;
+            current = e;
+            e.setIndex(0);
+            e.setNext(null);
+            size++;
+        }else{
+            e.setNext(current); //ссылка на прошлый э хранится в current, присваиваем next
+            e.setIndex(e.getIndex() + 1);
+            last = e;
+            current = e;
+            size++;
+        }
 
     }
 
